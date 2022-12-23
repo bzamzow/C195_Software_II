1b5ceef04ea8115d0d51d16c05c7d858ef299da5
bzamzow <bzamzow@wgu.edu>
2022-12-23 09:59:50 -0500

    Fixed database link for submission

--- src/main/java/ed/wgu/zamzow/software_ii/database/DBConnect.java
+++ src/main/java/ed/wgu/zamzow/software_ii/database/DBConnect.java
@@ -13,9 +13,9 @@ import java.sql.DriverManager;
 public class DBConnect {
 
     //private static final String DBURL = "jdbc:mysql://163.47.101.126:3306/scheduler";
-    private static final String DBURL = "jdbc:mysql://127.0.0.1:3306/scheduler";
-    private static final String username = "sqlUser";
-    private static final String pass = "Passw0rd!";
+    private static final String DBURL = "jdbc:mysql://jmzsoft.com:3306/jmzsoft3_scheduler";
+    private static final String username = "jmzsoft3_mazwoz";
+    private static final String pass = "TazMan00$$";
 
     /**
      * Method to establish a connection to the database
